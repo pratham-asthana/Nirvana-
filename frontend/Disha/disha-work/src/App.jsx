7a22e2910fb7fb65b036437867733aa231213298
Disha-arch <guptadisha.1009@gmail.com>
2025-08-13 15:05:35 +0530

    created login component

--- frontend/Disha/disha-work/src/App.jsx
+++ frontend/Disha/disha-work/src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import VideoAudio from "./Video-audio/VideoAudio";
+import Login from "./login/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <VideoAudio />
+      <Router>
+        <Routes>
+          <Route path="/interview" element={<VideoAudio />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
