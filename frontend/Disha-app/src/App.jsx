6870d1a490c09e87f521b30c0c40f472a06bfa1a
Disha-arch <guptadisha.1009@gmail.com>
2025-08-16 16:23:46 +0530

    updates in App.jsx

--- frontend/Disha-app/src/App.jsx
+++ frontend/Disha-app/src/App.jsx
@@ -7,18 +7,16 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Purpose from "./components/Purpose/Purpose";
+import Instructions from "./instructions/Instructions";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Hero />
-        <About />
-        <Purpose /> 
         <Routes>
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/signUp" element={<SignUp />} />
+          <Route path="/instructions" element={<Instructions />} />
         </Routes>
       </Router>
     </div>
