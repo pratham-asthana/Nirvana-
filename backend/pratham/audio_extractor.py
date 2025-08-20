0d54a66b2eff6037d1a22e5a2dc1cde7438c8fe8
Pratham Asthana <prathamasthana04@gmail.com>
2025-08-20 11:50:35 +0530

    commit

--- backend/pratham/audio_extractor.py
+++
@@ -1,5 +0,0 @@
-import ffmpeg
-import os
-
-def extract_audio(video_path, audio_path):
-    ffmpeg.input(video_path).output(audio_path, ac=1, ar='16000').run(overwrite_output=True)
--- backend/pratham/audio_transcriber.py
+++
@@ -1,5 +0,0 @@
-import whisper 
-def transcribe_audio(audio_path):
-    model = whisper.load_model("base")
-    transcription = model.transcribe(audio_path, language="en", task="transcribe")
-    return transcription['text']
