a84826fb7ffa9c6d451003faf9ae86837284429b
Disha-arch <guptadisha.1009@gmail.com>
2025-08-20 16:28:07 +0530

    updates

--- frontend/Disha-app/src/domain/Domain.jsx
+++ frontend/Disha-app/src/domain/Domain.jsx
@@ -14,22 +14,22 @@ const Domain = () => {
           innovation.
         </p>
       </div>
-      <div>
-        <div>
-          <IoCode />
+      <div className="domain-card-div">
+        <div className="domain-card-icon">
+          <IoCode size={"20px"} color="white" />
         </div>
-        <h3>Web Development</h3>
-        <p>
+        <h3 className="domain-name">Web Development</h3>
+        <p className="domain-description">
           Build modern, responsive web applications using cutting-edge
           technologies and frameworks.
         </p>
-        <div>
-          <p>2-5 years</p>
-          <p>$700 - $120,000</p>
+        <div className="domain-card-details">
+          <p className="domain-card-details-1">2-5 years</p>
+          <p className="domain-card-details-2">$700 - $120,000</p>
         </div>
-        <div>
-          <button>View Job Description</button>
-          <FaArrowRightLong />
+        <div className="domain-card-button-arrow">
+          <button className="view-jd-button">View Job Description</button>
+          <FaArrowRightLong className="arrow-icon" />
         </div>
       </div>
     </div>
