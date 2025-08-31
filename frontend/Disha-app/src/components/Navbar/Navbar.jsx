e0e7f0a3d35cc7f2889899621b30462068dd1120
Aryan Verma <aryanv090803@gmail.com>
2025-08-31 13:34:00 +0530

    Navbar Fixed

--- frontend/Disha-app/src/components/Navbar/Navbar.jsx
+++ frontend/Disha-app/src/components/Navbar/Navbar.jsx
@@ -6,30 +6,25 @@ const Navbar = () => {
   return (
     <header>
       <nav className="navbar">
-        <div className="logo">AI Interviewer</div>
-
-        <ul className="nav-links">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#purpose">Purpose</a>
-          </li>
-          <li>
-            <a href="#creators">Creators</a>
-          </li>
-        </ul>
-        <div className="nav-buttons">
-          <Link to="/signUp" className="btn btn-outline">
-            Sign Up
-          </Link>
-          <Link to="signIn" className="btn btn-primary">
-            Sign In
-          </Link>
-        </div>
+          <div className="logo">AI Interviewer</div>
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Home</Link>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#purpose">Purpose</a>
+              </li>
+              <li>
+                <a href="#creators">Creators</a>
+              </li>
+              </ul>
+              <div className="nav-buttons">
+                <Link to="/signUp" className="btn btn-outline">Sign Up</Link>
+                <Link to="signIn" className="btn btn-primary">Sign In</Link>
+              </div>
       </nav>
     </header>
   );
